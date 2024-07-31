6a9b8470dff8da0308e35da5c2937a02e95cca67
Julia Alves <juliaalvessabino6@gmail.com>
2024-07-31 10:18:26 -0300

    configuração do env

--- src/app/services/feed-rss.service.ts
+++ src/app/services/feed-rss.service.ts
@@ -2,20 +2,21 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable, take } from "rxjs";
 import { FeedRss } from "../interfaces/feedRss";
+import { environment } from "src/enviroment/environment";
 
 @Injectable({
     providedIn: 'root'
   })
   export class RssFeedService {
   
-    private readonly api: string = `http://localhost:3000/api/feed-rss`;
+    private readonly api: string = environment.apiUrl;
   
     constructor(
       private httpClient: HttpClient
     ) { }
   
     getNoticia(): Observable<FeedRss[]> {
-        return this.httpClient.get<FeedRss[]>(`${this.api}/noticias`)
+        return this.httpClient.get<FeedRss[]>(`${this.api}/feed-rss/noticias`)
         .pipe(take(1));
       }
   }
---
+++ src/enviroment/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: true,
+    apiUrl: 'https://doe-sangue-one.vercel.app' 
+  };
+  
